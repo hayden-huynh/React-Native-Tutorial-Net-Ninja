cec766b07927d4eaf88c3a403405419b1939bab3
Hayden Huynh <huynhhai323@gmail.com>
2021-08-14 23:17:12 +0700

    Finished tutorial 03

--- App.js
+++ App.js
@@ -4,9 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    // View is similar to div 
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, World!</Text>
+      </View>
+      <View style={styles.body}>
+        {/* Child components do not inherit the styles of their parent */}
+        {/* One exception is Text component i.e. child Text components DO inherit styles of their parent Text component */}
+        <Text style={styles.boldText}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro, error. Voluptatum vitae iusto, sed ex tenetur provident voluptate impedit minima! <Text>This part will inherit</Text> </Text>
+        <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro, error. Voluptatum vitae iusto, sed ex tenetur provident voluptate impedit minima!</Text>
+        <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro, error. Voluptatum vitae iusto, sed ex tenetur provident voluptate impedit minima!</Text>
+      </View>
     </View>
   );
 }
@@ -18,4 +27,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: "pink",
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: "bold"
+  },
+  body: {
+    backgroundColor: "yellow",
+    padding: 20,
+    fontWeight: "bold",
+  }
 });
