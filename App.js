1cdf80b9acd7ac6f4cc989dc183c7672334f119a
Hayden Huynh <huynhhai323@gmail.com>
2021-08-15 01:05:23 +0700

    Finished tutorial 07

--- App.js
+++ App.js
@@ -1,26 +1,39 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function App() {
   const [people, setPeople] = useState([
-    { name: "hayden", key: 1 },
-    { name: "yoshi", key: 2 },
-    { name: "mario", key: 3 },
-    { name: "luigi", key: 4 },
-    { name: "peach", key: 5 },
-    { name: "toad", key: 6 },
-    { name: "bowser", key: 7 },
+    { name: "hayden", id: "1" },
+    { name: "yoshi", id: "2" },
+    { name: "mario", id: "3" },
+    { name: "luigi", id: "4" },
+    { name: "peach", id: "5" },
+    { name: "toad", id: "6" },
+    { name: "bowser", id: "7" },
   ]);
 
   return (
     <View style={styles.container}>
-      <ScrollView>
+      {/* FlatList are scrollable by default */}
+      <FlatList
+        data={people}
+        keyExtractor={(item) => item.id} // If the elements don't have a "key" property but some other property that can act like a key, we can tell FlatList to use that property instead using keyExtractor prop
+        numColumns={2} // Number of columns to split the list up into
+        renderItem={({item}) => {
+          // Need to destructure to get the "item" property, which is the actual element in the array we assign to the "data" prop
+          // FlatList automatically looks for the "key" property in each of the array element so we don't have to set them manually
+          return <Text style={styles.item}>{item.name}</Text>
+        }}
+      />
+
+      {/* FlatList renders an element when it should be visible on the screen, whereas ScrollView always renders everything even when out of the screen, which may be inefficient and not suitable for long arrays */}
+      {/* <ScrollView>
         {people.map((item) => (
           <View key={item.key}>
             <Text style={styles.item}>{item.name}</Text>
           </View>
         ))}
-      </ScrollView>
+      </ScrollView> */}
     </View>
   );
 }
@@ -39,5 +52,7 @@ const styles = StyleSheet.create({
     padding: 30,
     backgroundColor: "pink",
     fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24
   },
 });
