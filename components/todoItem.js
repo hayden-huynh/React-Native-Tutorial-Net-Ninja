d7c25086e3dd0a21af7c97b871adb78c258ea825
Hayden Huynh <huynhhai323@gmail.com>
2021-08-21 23:38:58 +0700

    Finished tutorial 15

--- components/todoItem.js
+++ components/todoItem.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons"; // vector-icons are installed by default when a project is created using "expo init". Visit https://icons.expo.fyi to view all available icons
 
-export default function TodoItem({item, pressHandler}) {
+export default function TodoItem({ item, pressHandler }) {
   return (
     <TouchableOpacity onPress={() => pressHandler(item.key)}>
-      <Text style={styles.item}>{item.text}</Text>
+      <View style={styles.item}>
+        {/* Specify an icon using the name prop. Check this name at the link provided above */}
+        <MaterialIcons name="delete" size={18} color="#333" />
+        <Text style={styles.text}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 }
@@ -17,5 +22,9 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderStyle: "dashed",
     borderRadius: 10,
+    flexDirection: "row"
+  },
+  text: {
+    marginLeft: 10,
   }
 });
