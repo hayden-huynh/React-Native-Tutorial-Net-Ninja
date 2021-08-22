a3ca65d1fbdc8e8e68b1eb9638f1c66f0240496e
Hayden Huynh <huynhhai323@gmail.com>
2021-08-22 23:41:27 +0700

    Finished tutorial 21

--- gamezone/screens/home.js
+++ gamezone/screens/home.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 
-export default function Home() {
+// Any component that is set as a Screen in the Navigation Stack will automatically receive the "navigation" prop
+export default function Home({navigation}) {
+  const pressHandler = () => {
+    navigation.navigate("ReviewDetails"); // The param must be same as the name prop set to Screen components inside Navigation Stack
+
+    // navigation.push("ReviewDetails"); // push does the same job as navigate except that navigate will go to a Screen if it already exists in the stack, whereare push always adds another new Screen to the top of stack regardless of it existing or not
+  }
+
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Home Screen</Text>
+      <Button title="Go to review details" onPress={pressHandler}/>
     </View>
   );
 }
--- gamezone/screens/reviewDetails.js
+++ gamezone/screens/reviewDetails.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 
-export default function ReviewDetails() {
+export default function ReviewDetails({navigation}) {
+  const pressHandler = () => {
+    navigation.goBack(); // pops the current Screen off the Navigation Stack and returns to the immediately previous Screen in the stack
+  }
+
   return (
     <View style={globalStyles.container}>
       <Text>ReviewDetails Screen</Text>
+      <Button title="Back to Home screen" onPress={pressHandler}/>
     </View>
   );
 }
