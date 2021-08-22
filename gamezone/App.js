57fdb5eb1a9e0e93358d650c33b7e2827588f641
Hayden Huynh <huynhhai323@gmail.com>
2021-08-22 18:45:05 +0700

    Finished tutorial 17

--- gamezone/App.js
+++ gamezone/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import Home from "./screens/home"
+import React, { useState } from "react";
+import Home from "./screens/home";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+// These fonts can be used anywhere, in any components inside the app after being loaded
+const getFonts = () =>
+  Font.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
 
 export default function App() {
-  return (
-    <Home />
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return (
+      <Home />
+    );
+  } else {
+    // AppLoading provides a callback to be executed when some task is completed
+    return (
+      <AppLoading      
+        startAsync={getFonts} // The asynchronous task to be completed
+        onFinish={() => setFontsLoaded(true)} // Callback to execute when task is done
+        onError={console.warn} // Must provide onError when startAsync is used
+      />
+    );
+  }
 }
