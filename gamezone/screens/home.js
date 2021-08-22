547a2210fd7303f9d546c9e32e18305a07b82b6f
Hayden Huynh <huynhhai323@gmail.com>
2021-08-23 00:00:58 +0700

    Finished tutorial 22

--- gamezone/screens/home.js
+++ gamezone/screens/home.js
@@ -1,19 +1,46 @@
-import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import { globalStyles } from "../styles/global";
 
-// Any component that is set as a Screen in the Navigation Stack will automatically receive the "navigation" prop
-export default function Home({navigation}) {
-  const pressHandler = () => {
-    navigation.navigate("ReviewDetails"); // The param must be same as the name prop set to Screen components inside Navigation Stack
-
-    // navigation.push("ReviewDetails"); // push does the same job as navigate except that navigate will go to a Screen if it already exists in the stack, whereare push always adds another new Screen to the top of stack regardless of it existing or not
-  }
-
+export default function Home({ navigation }) {
+  const [reviews, setReviews] = useState([
+    {
+      title: "Zelda, Breath of Fresh Air",
+      rating: 5,
+      body: "lorem ipsum",
+      key: "1",
+    },
+    {
+      title: "Gotta Catch Them All (again)",
+      rating: 4,
+      body: "lorem ipsum",
+      key: "2",
+    },
+    {
+      title: 'Not So "Final" Fantasy',
+      rating: 3,
+      body: "lorem ipsum",
+      key: "3",
+    },
+  ]);
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Home Screen</Text>
-      <Button title="Go to review details" onPress={pressHandler}/>
+      <FlatList
+        data={reviews}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("ReviewDetails", item)} // The second param of navigate is the data to be passed to the destination screen. This data should be an object. In this case, item is already the object we want to pass.
+          >
+            <Text style={globalStyles.titleText}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
--- gamezone/screens/reviewDetails.js
+++ gamezone/screens/reviewDetails.js
@@ -2,15 +2,15 @@ import React from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 
-export default function ReviewDetails({navigation}) {
-  const pressHandler = () => {
-    navigation.goBack(); // pops the current Screen off the Navigation Stack and returns to the immediately previous Screen in the stack
-  }
+// Every Screen component of a Navigation Stack also receives a route prop automatically
+export default function ReviewDetails({ navigation, route }) {
+  const { title, body, rating } = route.params; // Accessing the params parameter of route will give us the data object passed over from the source Screen
 
   return (
     <View style={globalStyles.container}>
-      <Text>ReviewDetails Screen</Text>
-      <Button title="Back to Home screen" onPress={pressHandler}/>
+      <Text>{title}</Text>
+      <Text>{body}</Text>
+      <Text>{rating}</Text>
     </View>
   );
 }
